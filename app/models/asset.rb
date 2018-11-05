59af19585b2ed1efc07e1795383251601e3fd77d
Srinidhi Prabandham <srinidhi.prabandham@ephicacy.in>
2018-11-05 18:25:40 +0530

    Segrating assets

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,6 +5,18 @@ class Asset < ApplicationRecord
 
   before_validation :set_name
 
+  def kind
+    content_type = attachment.content_type
+    if content_type.include?("application/ecmascript") || name.include?(".js")
+      "js"
+    elsif content_type.include?("css") || name.include?(".css")
+      "css"
+    elsif content_type.include?("image")
+      "image"
+    else
+      "others"
+    end
+  end
   private
 
   def set_name
