70fee070f0ffaaaa40907f9580f5d830f0ad76f6
Srinidhi Prabandham <prabandham@larks.in>
2018-11-10 14:27:53 +0530

    Settings for puma

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,8 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
+bind 'unix:///var/run/puma.sock'
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -21,7 +22,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 5 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -29,6 +30,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # process behavior so workers use less memory.
 #
 # preload_app!
+daemonize true
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
