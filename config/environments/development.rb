2ae219c64cf5aba6c614099ac52e2746882a64d8
Srinidhi Prabandam <srinidhi.prabandam@cerner.com>
2021-06-29 20:53:48 +0530

    removing hardcoded password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,18 +60,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-
-  # Action Mailer configurations
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'sendinblue.com' }
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendinbule.com",
-    port: 587,
-    user_name: "larks.india@gmail.com ",
-    password: "sk5tb8fpJdWmEKOX",
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
 end
